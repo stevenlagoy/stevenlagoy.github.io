dcec126aca908ebcf62998c7aacca04a781dca4d
stevenlagoy <stevenlagoy@gmail.com>
2025-05-15 18:16:30 -0400

    Add Pokemon Grind Locator, rearrange files to facilitate this

---
+++ poke-grind-locator/FileLocations.ts
@@ -0,0 +1,4 @@
+export const pokemonJSONPath = "pokemon.json";
+export const movesJSONPath   = "moves.json";
+export const routesJSONPath  = "routes.json";
+export const effectivenessJSONPath = "effectiveness.json";
---
+++ poke-grind-locator/dist/Move.js
@@ -0,0 +1,9 @@
+export class Move {
+    constructor(accuracy, name, power, pp, type) {
+        this.accuracy = accuracy;
+        this.name = name;
+        this.power = power;
+        this.pp = pp;
+        this.type = type;
+    }
+}
