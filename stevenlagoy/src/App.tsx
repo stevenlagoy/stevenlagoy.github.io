809b57a00cb584f2e38533f21a3aa158b2563bc7
stevenlagoy <stevenlagoy@gmail.com>
2025-11-20 01:52:48 -0500

    Remove testing with resizing header

--- stevenlagoy/src/App.tsx
+++ stevenlagoy/src/App.tsx
@@ -1,36 +1,20 @@
 import './App.scss';
-import { useEffect, type ReactNode } from 'react';
+import { type ReactNode } from 'react';
 import Header from '@components/Header';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from "@pages/HomePage";
 import TwochrePage from '@pages/TwochrePage/TwochrePage';
 import FractalPage from '@pages/FractalPage';
 
-const Layout = ({ children }: { children: ReactNode }) => {
-  useEffect(() => {
-    const header = document.querySelector("header");
-    const main = document.querySelector("main");
-    const updateMargin = () => {
-      if (header && main) {
-        main.style.marginTop = `${header.getBoundingClientRect().height}px`;
-      }
-    };
-    updateMargin();
-    window.addEventListener("scroll", updateMargin);
-    window.addEventListener("resize", updateMargin);
-    return () => {
-      window.removeEventListener("scroll", updateMargin);
-      window.removeEventListener("resize", updateMargin);
-    };
-  }, []);
+const Layout = ({ children }: { children: ReactNode }) => {    
   
   return (
-    <div>
-      {/* <Header /> */}
+    <>
+      <Header />
       <main>
         { children }
       </main>
-    </div>
+    </>
   );
 };
 
