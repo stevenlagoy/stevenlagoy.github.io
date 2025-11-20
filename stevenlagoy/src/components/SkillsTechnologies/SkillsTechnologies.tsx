8ef631969f9eb6efb494c302b4a63d73dcadd028
stevenlagoy <stevenlagoy@gmail.com>
2025-11-20 01:56:06 -0500

    Add SkillsTechnologies component. Should read data from the skills_technologies file and populate the component from the data.

---
+++ stevenlagoy/src/components/SkillsTechnologies/SkillsTechnologies.tsx
@@ -0,0 +1,8 @@
+import styles from "./SkillsTechnologies.module.scss";
+
+export default function SkillsTechnologies() {
+    return (
+        <div className={styles.skillsTechnologies}>
+        </div>
+    );
+}
---
+++ stevenlagoy/src/data/skills_technologies.ts
@@ -0,0 +1,99 @@
+// https://www.svgrepo.com/svg/535245/c
+import cLogo from "@assets/icons/c-svgrepo-com.svg";
+// https://www.svgrepo.com/svg/508906/cplusplus
+import cppLogo from "@assets/icons/cplusplus-svgrepo-com.svg";
+// https://www.svgrepo.com/svg/508921/java
+import javaLogo from "@assets/icons/java-svgrepo-com.svg";
+// https://www.svgrepo.com/svg/473670/javascript
+import jsLogo from "@assets/icons/javascript-155-svgrepo-com.svg";
+// https://www.svgrepo.com/svg/342317/typescript
+import tsLogo from "@assets/icons/typescript-svgrepo-com.svg";
+// https://www.svgrepo.com/svg/508940/ruby
+import rubyLogo from "@assets/icons/ruby-svgrepo-com.svg";
+// https://www.svgrepo.com/svg/535339/database
+import sqlLogo from "@assets/icons/database-svgrepo-com.svg";
+// https://www.svgrepo.com/svg/473760/postgresql
+import postgresLogo from "@assets/icons/postgresql-svgrepo-com.svg";
+// https://www.svgrepo.com/svg/473729/mongodb
+import mongodbLogo from "@assets/icons/mongodb-svgrepo-com.svg";
+// https://www.svgrepo.com/svg/508795/css3-02
+import cssLogo from "@assets/icons/css3-02-svgrepo-com.svg";
+//https://www.svgrepo.com/svg/503555/sass
+import sassLogo from "@assets/icons/sass-svgrepo-com.svg";
+// https://www.svgrepo.com/svg/512355/html-124
+import htmlLogo from "@assets/icons/html-124-svgrepo-com.svg";
+// https://www.svgrepo.com/svg/512738/python-127
+import pythonLogo from "@assets/icons/python-svgrepo-com.svg"
+// https://www.svgrepo.com/svg/503536/react
+import reactLogo from "@assets/icons/react-svgrepo-com.svg";
+// https://www.svgrepo.com/svg/473589/docker
+import dockerLogo from "@assets/icons/docker-svgrepo-com.svg";
+
+export const skills_technologies = {
+    "practiced" : [
+        {
+            img: javaLogo,
+            name: "Java",
+        },
+        {
+            img: cLogo,
+            name: "C",
+        },
+        {
+            img: cppLogo,
+            name: "C++",
+        },
+        {
+            img: pythonLogo,
+            name: "Python",
+        },
+        {
+            img: htmlLogo,
+            name: "HTML5",
+        },
+        {
+            img: cssLogo,
+            name: "CSS",
+        },
+        {
+            img: sassLogo,
+            name: "Sass",
+        },
+        {
+            img: jsLogo,
+            name: "JavaScript",
+        },
+    ],
+    "familiar" : [
+        {
+            img: tsLogo,
+            name: "TypeScript",
+        },
+        {
+            img: rubyLogo,
+            name: "Ruby",
+        },
+        {
+            img: sqlLogo,
+            name: "SQL",
+        },
+        {
+            img: reactLogo,
+            name: "React",
+        },
+    ],
+    "exploring" : [
+        {
+            img: dockerLogo,
+            name: "Docker",
+        },
+        {
+            img: mongodbLogo,
+            name: "MongoDB",
+        },
+        {
+            img: postgresLogo,
+            name: "PostgreSQL",
+        },
+    ]
+}
