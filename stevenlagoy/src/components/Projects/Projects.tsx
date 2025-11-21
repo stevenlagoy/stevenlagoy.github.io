6d04676d964287ada1c55e1859cb31b799027f1c
stevenlagoy <stevenlagoy@gmail.com>
2025-11-21 03:00:17 -0500

    Create containers for projects based on project type

--- stevenlagoy/src/components/Projects/Projects.tsx
+++ stevenlagoy/src/components/Projects/Projects.tsx
@@ -2,21 +2,38 @@ import styles from "./Projects.module.scss";
 
 import ProjectCard from "@/components/ProjectCard";
 import { projects } from "@data/projects";
+import { useEffect, useState } from "react";
 
 export default function Projects() {
+    const [categories, setCategories] = useState<Set<string>>(() => new Set());
+
+    useEffect(() => {
+        projects.forEach(p => setCategories(prev => { return new Set(prev).add(p.type)}));
+    }, [projects]);
+
     return (
         <div className={styles.projects}>
-            {projects.map((project, idx) => (
-                <ProjectCard
-                    key={idx}
-                    imgSrc={project.imgSrc}
-                    projectName={project.projectName}
-                    projectDate={project.projectDate}
-                    technologies={project.technologies}
-                    projectDesc={project.projectDesc}
-                    projectPath={project.projectPath}
-                />
-            ))}
+            <h1>Projects</h1>
+            <div className={styles.projectsContainers}>
+                {[...categories].map((category: string, idx: number) => (
+                    <div className={styles.categoryContainer} key={idx}>
+                        <h2>{category}</h2>
+                        <div className={styles.categoryProjects}>
+                            {projects.filter(p => p.type === category).map((project, jdx) => (
+                                <ProjectCard
+                                    key={jdx}
+                                    imgSrc={project.imgSrc}
+                                    projectName={project.projectName}
+                                    projectDate={project.projectDate}
+                                    technologies={project.technologies}
+                                    projectDesc={project.projectDesc}
+                                    projectPath={project.projectPath}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 }
